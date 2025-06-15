437d32a98eb886ce7db1a2ce848a5aa28fcc955e
Daniil Surmin <hekache2@gmail.com>
2025-06-15 19:24:06 +0500

    task/0021 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val textWatcher  = object : SimpleTextWatcher() {
+
+            override fun afterTextChanged(s: Editable?) {
+                binding.actionButton.isEnabled = binding.inputEditText.text.toString().length >= 3
+            }
+        }
+
+        binding.actionButton.setOnClickListener {
+            binding.titleTextView.text = binding.inputEditText.text
+            binding.inputEditText.setText("")
+        }
+
+        binding.inputEditText.addTextChangedListener (textWatcher)
     }
+}
+private abstract class SimpleTextWatcher : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+    override fun afterTextChanged(s: Editable?) = Unit
+
 }
