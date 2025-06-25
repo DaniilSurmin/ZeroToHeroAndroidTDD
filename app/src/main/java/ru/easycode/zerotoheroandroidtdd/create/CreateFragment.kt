b6ff1ffaddfd4d820b188876ab2d996274c51f78
Daniil Surmin <hekache2@gmail.com>
2025-06-25 15:20:26 +0500

    task/0025 fix CreateFragment

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/create/CreateFragment.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/create/CreateFragment.kt
@@ -15,13 +15,17 @@ class CreateFragment : AbstractFragment<FragmentCreateBinding>() {
     override fun bind(inflater: LayoutInflater, container: ViewGroup?) =
         FragmentCreateBinding.inflate(inflater, container, false)
 
+    private lateinit var viewModel: CreateViewModel
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() = viewModel.comeback()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = (activity as ProvideViewModel).viewModel(CreateViewModel::class.java)
 
-        requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() = viewModel.comeback()
-        })
+        requireActivity().onBackPressedDispatcher.addCallback(onBackPressedCallback)
 
         binding.inputEditText.addTextChangedListener {
             binding.createButton.isEnabled = binding.inputEditText.text.toString().length >= 3
@@ -31,5 +35,10 @@ class CreateFragment : AbstractFragment<FragmentCreateBinding>() {
             hideKeyBoard()
             viewModel.add(binding.inputEditText.text.toString())
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        onBackPressedCallback.remove()
     }
 }
